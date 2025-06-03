0a9a37f0713871c196add550c3e4f26e1ee99b4e
Ankita896 <ankita143ankyy@gmail.com>
2025-06-03 20:10:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstWord(s) {
-  // your code here
+    if (!s) return '';
+
+ 
+    if (!s.includes(' ')) return s;
+
+
+    return s.slice(0, s.indexOf(' '));
+}
+
 }
 
 // Do not change the code below
